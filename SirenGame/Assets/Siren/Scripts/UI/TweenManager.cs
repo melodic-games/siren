76bd790cf70d6d9660f44954c9b4f17f6b4ac426
Maki <mxmcube@gmail.com>
2022-11-20 13:31:51 -0600

    Add more types to tween manager

--- SirenGame/Assets/Siren/Scripts/UI/TweenManager.cs
+++ SirenGame/Assets/Siren/Scripts/UI/TweenManager.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 
 public class TweenManager
 {
-    public class Tweener
+    public abstract class Tweener
     {
-        public float From;
-        public float To;
+        public virtual void Update()
+        {
+        }
+    }
+
+    public class Tweener<T> : Tweener
+    {
+        public T From;
+        public T To;
 
         private float _startTime;
         private float _endTime;
 
-        public float Current;
+        public T Current;
         public bool Finished = true;
 
         private EasingFunctions.Easing _easingFunction;
-        private readonly Action<float> _transitionFunction;
+        private readonly Action<T> _transitionFunction;
 
-        public Tweener(Action<float> transitionFunction, float initial = 0)
+        public Tweener(Action<T> transitionFunction, T initial)
         {
             _transitionFunction = transitionFunction;
             From = initial;
@@ -28,7 +35,7 @@ public class TweenManager
             transitionFunction(initial);
         }
 
-        public void Tween(float to, float ms, EasingFunctions.Easing easingFunction)
+        public void Tween(T to, float ms, EasingFunctions.Easing easingFunction)
         {
             From = Current;
             To = to;
@@ -40,7 +47,7 @@ public class TweenManager
             Finished = false;
         }
 
-        public void Update()
+        public override void Update()
         {
             if (Finished) return;
 
@@ -56,19 +63,33 @@ public class TweenManager
 
             var t = (Time.realtimeSinceStartup - _startTime) / duration;
             t = EasingFunctions.Ease(t, _easingFunction);
-
-            var n = Mathf.Lerp(From, To, t);
+            
+            // thanks you rider, i just hope this is optimized enough
+            var n = From switch
+            {
+                float fromFloat when To is float toFloat =>
+                    (T) Convert.ChangeType(Mathf.Lerp(fromFloat, toFloat, t), typeof(T)),
+                Color fromColor when To is Color toColor =>
+                    (T) Convert.ChangeType(Color.Lerp(fromColor, toColor, t), typeof(T)),
+                Vector2 fromVector2 when To is Vector2 toVector2 =>
+                    (T) Convert.ChangeType(Vector2.Lerp(fromVector2, toVector2, t), typeof(T)),
+                Vector3 fromVector3 when To is Vector3 toVector3 =>
+                    (T) Convert.ChangeType(Vector3.Lerp(fromVector3, toVector3, t), typeof(T)),
+                Quaternion fromQuaternion when To is Quaternion toQuaternion =>
+                    (T) Convert.ChangeType(Quaternion.Lerp(fromQuaternion, toQuaternion, t), typeof(T)),
+                _ => default
+            };
 
             _transitionFunction(n);
             Current = n;
         }
     }
 
-    private List<Tweener> _tweeners = new();
+    private readonly List<Tweener> _tweeners = new();
 
-    public Tweener NewTweener(Action<float> transition, float initial = 0)
+    public Tweener<T> NewTweener<T>(Action<T> transition, T initial)
     {
-        var tweener = new Tweener(transition, initial);
+        var tweener = new Tweener<T>(transition, initial);
         _tweeners.Add(tweener);
         return tweener;
     }
